d68e0eb4d7dd43003526dd4bf8c83fd603b4ebde
Samuel Martins <samuel.ubuntu@gmail.com>
2024-04-03 14:38:31 -0300

    Date validation on frontend

--- apps/student-calendar/src/environments/environment.ts
+++ apps/student-calendar/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  apiUrl: 'http://backend-bj3yk2bxdq-uc.a.run.app/api',
+  apiUrl: 'https://backend-bj3yk2bxdq-uc.a.run.app/api',
   baseurl: 'https://tarefas-app-exemplo.web.app',
   vapidKey: 'BHNfDPPNrY_Sui4l6IQs6-wJ2goQXchb4ujZTPsYaM7KmevtNo3EeqThVkGa2zMMSPftw2eklgwzipAe6gbD6UI'
 };
