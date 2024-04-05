b0042c442b671551d33efacf0521efcbecf29e37
Samuel Martins <samuel.ubuntu@gmail.com>
2024-04-04 23:13:30 -0300

    fixing fcm token update

--- apps/student-calendar-backend/src/app/users/users.service.ts
+++ apps/student-calendar-backend/src/app/users/users.service.ts
@@ -14,14 +14,14 @@ export class UsersService {
     return this.repo.getByUserId(userId);
   }
 
-  async createOrUpdateUser(userId: string, user: Partial<Omit<User, 'id' | 'userId' | 'createdAt'| 'updatedAt'>>) {
+  async createOrUpdateUser(userId: string, user: Partial<Omit<User, 'id' | 'userId' | 'createdAt' | 'updatedAt'>>) {
     const authUser = await auth().getUser(userId);
     const userToUpdate = {
       displayName: user.name ?? authUser.displayName,
 
     };
     await auth().updateUser(userId, userToUpdate);
-    return this.repo.createOrUpdateUser(userId, {...user, photoUrl: '', userId, fcmToken: ''});
+    return this.repo.createOrUpdateUser(userId, { ...user, photoUrl: '', userId, fcmToken: user.fcmToken ?? '' });
   }
 
 
